aaa54285546246f6f2194bc36f377869db9b7757
hydraturk3-eng <hydraturk3@gmail.com>
2026-06-18 16:24:48 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // هذا السطر يتجاهل أخطاء eslint أثناء البناء
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // هذا السطر يتجاهل أخطاء التايب سكريبت لتفادي أي توقف آخر
-    ignoreBuildErrors: true,
-  },
+  /* خيارات الإعدادات هنا إذا لزم الأمر */
 };
 
 export default nextConfig;
